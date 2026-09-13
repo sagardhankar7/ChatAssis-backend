a25241965543515413d40b1ebf226dbacf881d7d
sagardhankar7 <sag9315@gmail.com>
2026-09-13 16:25:01 +0530

    patch 3.1

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (!fs.existsSync(uploadDir)) {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/")
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname) // .pdf
